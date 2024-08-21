e107baffdd60b0c79d886d2a7875d52e88328a2a
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-08-20 23:36:24 -0500

    Install Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+</script>
+
+<nav class="grid grid-cols-4 row-end-auto gap-4 mt-4 ml-8 mb-4 font-sans text-2xl">
+    <a class="font-bold">SvelteKit Todo App</a>
+    <a href='/'>All Todo Items</a>
+    <a href='/'>Active Todo Items</a>
+    <a href='/'>Completed Todo Items</a>
+</nav>
+  
+  <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div>
+    <h1>Welcome to my SvelteKit Todo App!</h1>
+    <p>Work in Progress. Come back soon!</p>
+</div>
